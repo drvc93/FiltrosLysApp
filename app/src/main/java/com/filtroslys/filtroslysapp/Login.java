598b9c0ebdb83767df20db3471d5111f736f7796
dvillanueva <drvc93@hotmail.com>
2016-08-04 14:53:03 -0500

    cambios 04-08

--- app/src/main/java/com/filtroslys/filtroslysapp/Login.java
+++ app/src/main/java/com/filtroslys/filtroslysapp/Login.java
@@ -1,14 +1,47 @@
 package com.filtroslys.filtroslysapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Login extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_login, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Login");
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        assert actionBar != null;
+
+      //  actionBar.setLogo(R.drawable.iconolys36x36);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.Volver) {
+
+            super.onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
