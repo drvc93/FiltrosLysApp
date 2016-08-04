6ae2a99f66995a65e091a6b88ad5c3a3da2c9af5
dvillanueva <drvc93@hotmail.com>
2016-08-04 17:57:56 -0500

    ultimos cambios 04-08

--- app/src/main/java/com/filtroslys/filtroslysapp/Login.java
+++ app/src/main/java/com/filtroslys/filtroslysapp/Login.java
@@ -1,7 +1,10 @@
 package com.filtroslys.filtroslysapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +16,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import DataBase.AccesosDB;
+import DataBase.MenuDB;
+import DataBase.ProdMantDataBase;
+import Tasks.GetAccesosDataTask;
+import Tasks.GetMenuDataTask;
 
 public class Login extends AppCompatActivity {
 
@@ -71,8 +83,9 @@ public class Login extends AppCompatActivity {
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Login.this, MenuPrincipal.class);
-                startActivity(i);
+                ShowDialogAlert();
+                //Intent i = new Intent(Login.this, MenuPrincipal.class);
+                //startActivity(i);
             }
         });
 
@@ -88,5 +101,71 @@ public class Login extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public  void  ShowDialogAlert (){
+
+
+
+        new AlertDialog.Builder(Login.this)
+                .setTitle("Advertencia")
+                .setMessage("Para ingresar a la aplicación  móvil , primero se debe realizar la sincronizacion")
+                .setIcon(R.drawable.icn_alert)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                       SincMenuAcceso();
+                    }
+                }).show();
+
+
+    }
+
+    public  void SincMenuAcceso()  {
+
+
+
+        GetMenuDataTask getMenuDataTask = new GetMenuDataTask();
+        AsyncTask<String,String,ArrayList<MenuDB>> asyncTask;
+        ArrayList<MenuDB> menuDBs= new ArrayList<MenuDB>();
+        ProdMantDataBase db =  new ProdMantDataBase(Login.this);
+        db.deleteTables();
+
+
+        try {
+            asyncTask = getMenuDataTask.execute();
+            menuDBs= (ArrayList<MenuDB>)asyncTask.get();
+            for (int i = 0 ; i <menuDBs.size();i++){
+
+                MenuDB mn = menuDBs.get(i);
+                db.InsetrtMenus(mn);
+            }
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        ArrayList<AccesosDB> accesosDBs = new ArrayList<AccesosDB>();
+        GetAccesosDataTask getAccesosDataTask = new GetAccesosDataTask();
+        AsyncTask<String,String,ArrayList<AccesosDB>> asyncTaskAccesos;
+
+        try {
+            asyncTaskAccesos = getAccesosDataTask.execute();
+            accesosDBs = (ArrayList<AccesosDB>)asyncTaskAccesos.get();
+            for (int i = 0 ; i <accesosDBs.size() ; i++){
+                AccesosDB acdb =  accesosDBs.get(i);
+                db.InsertAccesos(acdb);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        //ShowProgressDialog(tipoSincro);
+
     }
 }
